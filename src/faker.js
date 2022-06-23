8064d55c540dd4ece83601bc8fefb6b84fb79241
Joséphine <j_bach@etu-webschoolfactory.fr>
2022-06-23 09:54:11 +0200

    [FEAT]: Faker & json-server :fire:

--- src/faker.js
+++ src/faker.js
@@ -1,23 +1,25 @@
 const faker = require("faker");
-faker.setLocale('fr');
-const fs = require("fs");const generatePersonsData = (number) => {
+faker.setLocale("fr");
+const fs = require("fs");
+const generatePersonsData = (number) => {
   const persons = [];
   while (number >= 0) {
     persons.push({
       id: number,
       title: faker.company.bsAdjective(),
       description: faker.commerce.productDescription(),
-      price:faker.commerce.price(100, 200, 0, '€'),
+      price: faker.commerce.price(100, 200, 0, "€"),
       picture: faker.image.sports(),
-      street:faker.address.streetAddress(),
-      city:faker.address.city(),
+      street: faker.address.streetAddress(),
+      city: faker.address.city(),
       country: faker.address.country(),
-      joining_date: faker.date.soon(30, '2020-01-01T00:00'),
+      joining_date: faker.date.soon(30, "2020-01-01T00:00"),
     });
     number--;
   }
   return persons;
-};fs.writeFileSync(
+};
+fs.writeFileSync(
   "./data/db.json",
   JSON.stringify({ users: generatePersonsData(20) })
 );
