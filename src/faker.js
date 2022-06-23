bb83202b0890d08aa13a4f91a8f0528fb1c59bf4
Joséphine <j_bach@etu-webschoolfactory.fr>
2022-06-23 09:52:51 +0200

    [FEAT]: Faker & json-server

---
+++ src/faker.js
@@ -0,0 +1,23 @@
+const faker = require("faker");
+faker.setLocale('fr');
+const fs = require("fs");const generatePersonsData = (number) => {
+  const persons = [];
+  while (number >= 0) {
+    persons.push({
+      id: number,
+      title: faker.company.bsAdjective(),
+      description: faker.commerce.productDescription(),
+      price:faker.commerce.price(100, 200, 0, '€'),
+      picture: faker.image.sports(),
+      street:faker.address.streetAddress(),
+      city:faker.address.city(),
+      country: faker.address.country(),
+      joining_date: faker.date.soon(30, '2020-01-01T00:00'),
+    });
+    number--;
+  }
+  return persons;
+};fs.writeFileSync(
+  "./data/db.json",
+  JSON.stringify({ users: generatePersonsData(20) })
+);
